22faee41c99c649d791f80084d5a5cfdf6002ad0
Tiago <tiagobnarita@gmail.com>
2024-04-26 19:51:18 -0300

    UAU

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito pizza</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - faça seu login</title>
+      </Head>
+      <div>
+        <h1>Sujeito Pizzaria</h1>
+      </div>
+    </>
   );
 }
