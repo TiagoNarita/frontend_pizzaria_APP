ee15c433cb07efc3104f85e28ddc99d6c07933e1
Tiago <tiagobnarita@gmail.com>
2024-05-03 00:28:18 -0300

    moious

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, ReactNode } from "react";
+
+type AuthContextData = {
+  user: UserProps;
+  isAuthenticated: boolean;
+  signIn: (credentials: SignInProps) => Promise<void>;
+};
+
+type UserProps = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+type SignInProps = {
+  email: string;
+  password: string;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+}
