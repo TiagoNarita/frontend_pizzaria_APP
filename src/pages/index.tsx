dd125da1dc141edcfaee19ca25315101f0b957e8
Tiago <tiagobnarita@gmail.com>
2024-05-03 20:05:49 -0300

    very difficult

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, FormEvent } from "react";
+
 import Head from "next/head";
 import styles from "../../styles/home.module.scss";
 import logoImg from "../../public/logo.svg";
@@ -5,9 +7,24 @@ import Image from "next/image";
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/button";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 import Link from "next/link";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: "tiago@teste.com",
+      password: "12345",
+    };
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -16,7 +33,7 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo sujeito pizzaria" />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu email" type="text" />
             <Input placeholder="Digite sua senha" type="password" />
             <Button loading={false}>Acessar</Button>
