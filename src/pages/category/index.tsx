e51799f10c62e82e2025ecab8de16590969688b8
Tiago <tiagobnarita@gmail.com>
2024-05-22 23:27:16 -0300

    category function

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,8 +1,20 @@
 import { Header } from "@/src/components/Header";
 import Head from "next/head";
 import styles from "./styles.module.scss";
+import { FormEvent, useState } from "react";
+import { setupAPIClient } from "@/src/services/api";
 
 export default function Category() {
+  const [name, setName] = useState("");
+
+  async function handleRegiste(event: FormEvent) {
+    event.preventDefault();
+
+    if (name === "") {
+      return;
+    }
+  }
+
   return (
     <>
       <Head>
@@ -13,14 +25,18 @@ export default function Category() {
         <main className={styles.container}>
           <h1>Cadastrar categorias</h1>
 
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleRegiste}>
             <input
               type="text"
               placeholder="Digite o nome da categoria"
               className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
 
-            <button type="submit">Cadastrar</button>
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar
+            </button>
           </form>
         </main>
       </div>
