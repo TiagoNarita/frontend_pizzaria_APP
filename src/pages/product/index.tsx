a4ee3f72833489a32a82f0a1d168515e189cb72e
Tiago <tiagobnarita@gmail.com>
2024-05-24 00:49:16 -0300

    style product

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -13,6 +13,33 @@ export default function Product() {
         <Header />
         <main className={styles.container}>
           <h1>Novo Produto</h1>
+
+          <form className={styles.form}>
+            <select>
+              <option>Bebida</option>
+              <option>Pizzas</option>
+            </select>
+
+            <input
+              type="text"
+              placeholder="Digite o nome do produto"
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Preço do produto"
+              className={styles.input}
+            />
+
+            <textarea
+              placeholder="Descreva seu produto"
+              className={styles.input}
+            />
+
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar
+            </button>
+          </form>
         </main>
       </div>
     </>
