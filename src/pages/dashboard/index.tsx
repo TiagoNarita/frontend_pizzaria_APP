6c2602bd7a62f80c8a87518d0867f4c486c81dd3
Tiago <tiagobnarita@gmail.com>
2024-05-28 21:03:19 -0300

    dashboard styles created

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/src/components/Header";
 import { canSSRAuth } from "@/src/utils/canSSRAuth";
 import Head from "next/head";
 import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
 
 export default function Dashboard() {
   return (
@@ -12,7 +13,21 @@ export default function Dashboard() {
       <div>
         <Header />
         <main className={styles.container}>
-          <div className={styles.containerHeader}></div>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa 30</span>
+              </button>
+            </section>
+          </article>
         </main>
       </div>
     </>
